303760cb6bf4673d0ee9118b9c43d346c8b79e74
Tailung1 <ramazsamitashvili11@gmail.com>
2025-05-04 23:06:52 +0400

    modify code structure

--- src/components/Planet.tsx
+++ src/components/Planet.tsx
@@ -8,9 +8,9 @@ export default function Planet() {
   const planetToShow =
     data.find((planet) => planet.name === planetName) || data[2];
 
-  const [info, SetInfo] = useState<"overview" | "structure" | "geology">(
-    "overview"
-  );
+  const [info, SetInfo] = useState<
+    "overview" | "structure" | "geology"
+  >("overview");
 
   const content = {
     overview: planetToShow?.overview.content,
@@ -19,36 +19,36 @@ export default function Planet() {
   };
 
   return (
-    <div className="flex flex-col  items-center mt-[127px]">
-      <div className="flex items-center justify-center   mb-[87px] gap-[220px]">
-        <div className="w-[400px] h-[400px] max-w-full max-h-full overflow-hidden flex justify-center items-center">
+    <div className='flex flex-col  items-center mt-[127px]'>
+      <div className='flex items-center justify-center   mb-[87px] gap-[220px]'>
+        <div className='w-[400px] h-[400px] max-w-full max-h-full overflow-hidden flex justify-center items-center'>
           <img
             src={planetToShow?.image}
             alt={planetToShow?.name}
             width={planetToShow?.width}
             height={planetToShow?.height}
-            className="object-contain object-center max-w-full max-h-full"
+            className='object-contain object-center max-w-full max-h-full'
           />
         </div>
 
-        <div className="flex flex-col items-start w-[350px]">
-          <h1 className="text-[#fff] text-[80px] font-bold">
+        <div className='flex flex-col items-start w-[350px]'>
+          <h1 className='text-[#fff] text-[80px] font-bold'>
             {planetToShow?.name}
           </h1>
 
-          <div className="h-[75px] flex items-center">
-            <p className="text-[#fff] text-[14px] font-normal leading-[25px]">
+          <div className='h-[75px] flex items-center'>
+            <p className='text-[#fff] text-[14px] font-normal leading-[25px]'>
               {content[info]}
             </p>
           </div>
 
-          <div className="Src">
+          <div className='Src'>
             <p>
-              Source : <span className="underline">Wikipedia</span>{" "}
+              Source : <span className='underline'>Wikipedia</span>{" "}
             </p>
           </div>
 
-          <div className="flex flex-col gap-[16px] mb-[56px] bg-}">
+          <div className='flex flex-col gap-[16px] mb-[56px] bg-}'>
             <button
               onClick={() => SetInfo("overview")}
               className={`Btn ${
@@ -56,41 +56,47 @@ export default function Planet() {
               }`}
             >
               {" "}
-              <span className="BtnSpan">01</span>Overview
+              <span className='BtnSpan'>01</span>Overview
             </button>
 
-            <button onClick={() => SetInfo("structure")} className="Btn">
+            <button
+              onClick={() => SetInfo("structure")}
+              className='Btn'
+            >
               {" "}
-              <span className="BtnSpan">02</span>INTERNAL STRUCTURE
+              <span className='BtnSpan'>02</span>INTERNAL STRUCTURE
             </button>
 
-            <button onClick={() => SetInfo("geology")} className="Btn">
+            <button
+              onClick={() => SetInfo("geology")}
+              className='Btn'
+            >
               {" "}
-              <span className="BtnSpan">03</span>
+              <span className='BtnSpan'>03</span>
               SURFACE GEOLOGY
             </button>
           </div>
         </div>
       </div>
-      <div className="flex gap-[30px] mb-[56px]">
-        <div className="Details">
-          <span className="DetailsSpan">ROTATION TIME</span>{" "}
-          <p className="DetailsP">{planetToShow?.rotation}</p>
+      <div className='flex gap-[30px] mb-[56px]'>
+        <div className='Details'>
+          <span className='DetailsSpan'>ROTATION TIME</span>{" "}
+          <p className='DetailsP'>{planetToShow?.rotation}</p>
         </div>
 
-        <div className="Details">
-          <span className="DetailsSpan">REVOLUTION TIME</span>{" "}
-          <p className="DetailsP">{planetToShow?.revolution}</p>
+        <div className='Details'>
+          <span className='DetailsSpan'>REVOLUTION TIME</span>{" "}
+          <p className='DetailsP'>{planetToShow?.revolution}</p>
         </div>
 
-        <div className="Details">
-          <span className="DetailsSpan">radius</span>{" "}
-          <p className="DetailsP">{planetToShow?.radius_km} KM</p>
+        <div className='Details'>
+          <span className='DetailsSpan'>radius</span>{" "}
+          <p className='DetailsP'>{planetToShow?.radius_km} KM</p>
         </div>
 
-        <div className="Details">
-          <span className="DetailsSpan">AVERAGE TEMP.</span>{" "}
-          <p className="DetailsP">
+        <div className='Details'>
+          <span className='DetailsSpan'>AVERAGE TEMP.</span>{" "}
+          <p className='DetailsP'>
             {planetToShow?.temperature_c.average}°c{" "}
           </p>
         </div>
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -3,33 +3,33 @@ import Header from "../components/Header";
 import { useEffect } from "react";
 
 export default function Layout() {
-   const navigateLinks = [
-      "/Mercury",
-      "/Venus",
-      "/Earth",
-      "/Mars",
-      "/Jupiter",
-      "/Saturn",
-      "/Uranus",
-      "/Neptune",
-   ];
-   const navigate = useNavigate();
-   const location = useLocation();
-   const pathName = location.pathname;
+  const navigateLinks = [
+    "/Mercury",
+    "/Venus",
+    "/Earth",
+    "/Mars",
+    "/Jupiter",
+    "/Saturn",
+    "/Uranus",
+    "/Neptune",
+  ];
+  const navigate = useNavigate();
+  const location = useLocation();
+  const pathName = location.pathname;
 
-   useEffect(() => {
-      if (navigateLinks.includes(pathName)) {
-         navigate(pathName);
-      } else {
-         navigate("/Earth");
-      }
-   }, []);
-   // Programetically
+  useEffect(() => {
+    if (navigateLinks.includes(pathName)) {
+      navigate(pathName);
+    } else {
+      navigate("/Earth");
+    }
+  }, []);
+  // Programetically
 
-   return (
-      <>
-         <Header />
-         <Outlet />
-      </>
-   );
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
 }
